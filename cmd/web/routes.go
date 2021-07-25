214a5ebbfd6848945f08a9c95eac64a837e11f34
alvinashiatey <alvinashiatey@gmail.com>
2021-07-25 19:50:34 -0400

    update

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/search-availability", handlers.Repo.Reservation)
+	mux.Get("/paynin", handlers.Repo.Paynin)
+	mux.Get("/kakra", handlers.Repo.Kakra)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -14,28 +14,44 @@ type Repository struct {
 	App *config.AppConfig
 }
 
-func NewRepo (a *config.AppConfig) *Repository {
-	return  &Repository {
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
 		a,
 	}
 }
 
-func NewHandlers (r *Repository){
+func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-func (m *Repository) Home (w http.ResponseWriter, r *http.Request){
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
-func (m *Repository) About(w http.ResponseWriter, r *http.Request)  {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hi from Alvin"
 	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Paynin(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "paynin.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Kakra(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "kakra.page.tmpl", &models.TemplateData{})
 }
